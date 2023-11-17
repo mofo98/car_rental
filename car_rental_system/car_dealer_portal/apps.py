8934128c82606eb7218db289d4db82503e324b5f
mofo98 <benjaminsimiyu051@gmail.com>
2023-11-17 13:27:05 +0300

    updates
    updates

---
+++ car_rental_system/car_dealer_portal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarDealerPortalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'car_dealer_portal'
--- car_rental_system/car_rental_system/settings.py
+++ car_rental_system/car_rental_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '8000',
     }
 }
 
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join("static"), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
