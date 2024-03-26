bfdc92ddf8d5ccd728597f4bd561054e9804e9cb
mofo98 <benjaminsimiyu051@gmail.com>
2024-03-26 10:10:29 +0300

    cache
    cache

--- car_rental_system/car_rental_system/urls.py
+++ car_rental_system/car_rental_system/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer_portal/',include('customer_portal.urls')),
@@ -24,3 +25,5 @@ urlpatterns = [
     path('',include('Home.urls')),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
